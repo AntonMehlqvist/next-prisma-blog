6ac5938665d3c2ed93c7abdfff0abae3ff3f4676
Anton Mehlqvist <antonmehlqvist@outlook.com>
2022-07-05 20:23:24 +0200

    fixed

--- src/server/router/example.ts
+++ src/server/router/example.ts
@@ -2,13 +2,6 @@ import { createRouter } from "./context";
 import { z } from "zod";
 
 export const exampleRouter = createRouter()
-  .query("hello", {
-    resolve({ input }) {
-      return {
-        greeting: `Hello ${input?.text ?? "world"}`,
-      };
-    },
-  })
   .query("getAll", {
     async resolve({ ctx }) {
       return await ctx.prisma.user.findMany();
